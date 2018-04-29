53cd031d39561476518f062c81b91005061d7183
sohailmerchant <13290932+sohailmerchant@users.noreply.github.com>
2018-04-29 21:52:19 +0100

    FinalDraft

--- SPO-ExternalUsers.ps1
+++ SPO-ExternalUsers.ps1
@@ -20,7 +20,7 @@ $ExternalUsers.Clear()
 foreach ($site in $SiteCollections) {
     [array]$ExternalUsers += Get-SPOUser -Limit All -Site $site.Url `
         | Where-Object {$_.LoginName -like "*urn:spo:guest*" -or $_.LoginName -like "*#ext#*"} `
-        | Select-Object DisplayName, LoginName, @{Name = "Url" ; Expression = { $site.Url }
+        | Select-Object DisplayName, LoginName, @{Name = "Site" ; Expression = { $site }
     }
 
 }
