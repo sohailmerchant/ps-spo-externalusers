9b08335a9de5fd549e291caa78d4b0720a1c7257
sohailmerchant <13290932+sohailmerchant@users.noreply.github.com>
2018-04-29 19:22:11 +0100

    draft

--- SPO-ExternalUsers.ps1
+++ SPO-ExternalUsers.ps1
@@ -1,21 +1,30 @@
+Param(
+    [Parameter(Mandatory = $False)]
+    [string]$AdminUrl = "https://sharepointinguk-admin.sharepoint.com/",
+	
+    [Parameter(Mandatory = $False)]
+    [string]$siteurl = "*"
+)
 
-
-$AdminUrl = "https://sharepointinguk-admin.sharepoint.com/"
 $cred = Get-AutomationPSCredential -Name 'SharePointingUKAdmin'
 Connect-SPOService -Url $AdminUrl $cred
 
-
 $siteurl = 'https://sharepointinguk.sharepoint.com/sites/admin'
 
-$SiteCollections  = Get-SPOSite -Identity $siteurl
-$ExternalUsers.Clear()
-foreach ($site in $SiteCollections)
-{
-	[array]$ExternalUsers += Get-SPOUser -Limit All -Site $site.Url `
-    | Where-Object {$_.LoginName -like "*urn:spo:guest*" -or $_.LoginName -like "*#ext#*"} `
-    | Select-Object DisplayName,AcceptedAs, InvitedBy,LoginName,@{Name = "Url" ; Expression = { $site.Url }
-	}
+if ($siteurl -eq "*") {
+    $SiteCollections = Get-SPOSite -Limit All
+}
+else {
+    $SiteCollections = Get-SPOSite -Identity $siteurl
 
-} 
+}
+$ExternalUsers.Clear()
+foreach ($site in $SiteCollections) {
+    [array]$ExternalUsers += Get-SPOUser -Limit All -Site $site.Url `
+        | Where-Object {$_.LoginName -like "*urn:spo:guest*" -or $_.LoginName -like "*#ext#*"} `
+        | Select-Object DisplayName, LoginName, @{Name = "Url" ; Expression = { $site.Url }
+    }
+
+}
 
 Write-Output ( $ExternalUsers | ConvertTo-Json)
